1c89581c652a593c93020a41fe1624daac2c397d
Lagomorph <godzilka@gmail.com>
2023-04-08 14:05:58 -0700

    continuing progress continuing progress

--- sll/sll1.js
+++ sll/sll1.js
@@ -1,3 +1,5 @@
+//This could be broken up into more files, rather an just dumping everything here, but I didn't feel like doing that. 
+
 class Node {
     constructor(data) {
         this.data = data;
@@ -33,7 +35,8 @@ class LinkedList {
         let runner=this.head;
         let result = "";
         while(runner !== null){
-            result += runner.data
+            runner === this.head ?result = runner.data
+            :result += `,${runner.data}`;
             runner = runner.next;
         }
         return result;
@@ -48,6 +51,91 @@ class LinkedList {
             runner = runner.next;
         }
         return res;
+    }
+    length() {
+        let runner = this.head;
+        let res = 0;
+        while(runner !== null){
+            res++;
+            runner = runner.next;
+        }
+        return res;
+    }
+    //For max, min, and avg, I choose to work just with numbers. It would have been a lot more simple to just make it assume numbers, but this was more interesting to me in the moment. I don't like the redundancy in the code however, and think there is probably a better way to do it. 
+    max() {
+        let runner = this.head;
+        let high = null;
+        while(runner !== null){
+            if(typeof(runner.data) === "number"){
+                if(high === null){
+                    high = runner.data;
+                }
+                else {
+                    if(high < runner.data){
+                        high = runner.data;
+                    }
+                }
+                runner = runner.next;
+            }
+            else{runner = runner.next;}
+        }
+        return high
+    }
+    min() {
+        let runner = this.head;
+        let low = null;
+        while(runner !== null){
+            if(typeof(runner.data) === "number"){
+                if(low === null){
+                    low = runner.data;
+                }
+                else {
+                    if(low > runner.data){
+                        low = runner.data;
+                    }
+                }
+                runner = runner.next;
+            }
+            else{runner = runner.next;}
+        }
+        return low
+    }
+    avg() {
+        let runner = this.head;
+        let sum = 0;
+        let count = 0;
+        while(runner !== null){
+            if(typeof(runner.data) === "number"){
+                sum += runner.data;
+                count++
+                runner = runner.next;
+            }
+            else{runner = runner.next;}
+        }
+        return sum/count;
+    }
+    last() {
+        let runner = this.head;
+        while(runner.next){
+            runner = runner.next;
+        }
+        return runner.data;
+    }
+    cabooseBeGone() {
+        let runner = this.head;
+        while(runner.next.next){
+            runner = runner.next;   
+        }
+        runner.next = null;
+        return this.display();
+    }
+    addABoose(val){
+        let runner = this.head;
+        while(runner.next){
+            runner = runner.next;
+        }
+        runner.next = new Node(val);
+        return this.display();
     }
 }
 
@@ -71,6 +159,15 @@ console.log(sll.findValue(6));
 console.log(sll.addFront(9));
 console.log(sll.display());
 console.log(sll.findValue(9));
-node3.next = new Node(" Howdy! ");
+node3.next = node4 = new Node("Howdy!");
 console.log(sll.display());
 console.log(sll.sum());
+console.log(sll.length());
+console.log(sll_empty.length());
+node4.next = node5 = new Node(-4);
+console.log(sll.max());
+console.log(sll.min());
+console.log(sll.avg());
+console.log(sll.last());
+console.log(sll.cabooseBeGone());
+console.log(sll.addABoose(-7));
