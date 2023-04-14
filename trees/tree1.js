cc0c7f77cc98eb2f330f1e57fe395483bb3b0871
Lagomorph <godzilka@gmail.com>
2023-04-13 17:34:10 -0700

    Adding tree algos

---
+++ trees/tree1.js
@@ -0,0 +1,128 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    //add value to tree
+    add(value) {
+        if(this.root) {
+            var runner = this.root;
+            while(runner) {
+                if(value>runner.value) {
+                    if(runner.right){
+                        runner = runner.right;
+                    } else {
+                        runner.right = new Node(value);
+                        return this
+                    }
+                } else {
+                    if(runner.left){
+                        runner = runner.left;
+                    } else {
+                        runner.left = new Node(value);
+                        return this
+                    }
+                }
+            }
+        }
+        this.root = new Node(value);
+        return this
+    }
+    //find if a value is in a tree
+    contains(value) {
+        var runner = this.root;
+        while (runner) {
+            if (value == runner.value) {
+                return true;
+            }
+            if (value < runner.value) {
+                if (!runner.left) {
+                    return false;
+                }
+                runner = runner.left;
+            } else {
+                if (!runner.right) {
+                    return false;
+                }
+                runner = runner.right;
+            }
+        }
+        return false;
+    }
+    //find the smallest value in a tree
+    min() {
+        var runner = this.root;
+        var min = this.root.value;
+        while(runner.left) {
+            if(runner.left.value < min) {
+                min = runner.left.value;
+            }
+            runner = runner.left;
+        }
+        return min
+    }
+    //find the largest value in a tree
+    max() {
+        var runner = this.root;
+        var max = this.root.value;
+        while(runner.right) {
+            if(runner.right.value > max) {
+                max = runner.right.value;
+            }
+            runner = runner.right;
+        }
+        return max
+    }
+    //find the number of nodes in a tree
+    size() {
+        if (this.root === null) {
+            return 0;
+        }
+        function sizeHelp(runner) {
+            if (!runner) {
+                return 0;
+            }
+            return 1 + sizeHelp(runner.left) + sizeHelp(runner.right);
+        }
+        return sizeHelp(this.root);
+    }
+    //see if the tree is empty of values
+    isEmpty() {
+        if(this.root) {
+            return false
+        }
+        return true
+    }
+}
+// creating a new BST
+tree = new BST();
+
+// adding some nodes
+tree.add(10).add(20).add(5).add(7).add(21).add(2).add(9).add(17).add(6).add(4)
+
+// searching for value
+console.log(`contains 17? ${tree.contains(17)}`)
+// searching for value that doesn't exist in the tree
+console.log(`contains 'hello'? ${tree.contains('hello')}`)
+
+// testing min
+console.log(`min val: ${tree.min()}`)
+// testing max
+console.log(`max val: ${tree.max()}`)
+
+// testing size
+console.log(`tree contians ${tree.size()} nodes`)
+
+//testing isEmpty
+console.log(tree.isEmpty())
+emptyTree = new BST();
+console.log(emptyTree.isEmpty())
+
+// printing our tree
+console.log(tree);
